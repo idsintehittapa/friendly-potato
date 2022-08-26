03e6b44c74068ddb4a83233bb550d4d123744db6
idsintehittapa <jblom15@gmail.com>
2022-08-26 12:09:35 +0200

    adding configed prettier, cleaning code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,39 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { MatDatepickerInputEvent } from '@angular/material/datepicker';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { MatDatepickerInputEvent } from '@angular/material/datepicker'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'Frontend Challenge - Johanna Blom';
+export class AppComponent implements OnInit {
+  title = 'Frontend Challenge - Johanna Blom'
 
   // Title of event
-  public eventTitle: any = '';
+  public eventTitle: any = ''
   titleEvent(event: any) {
-    this.eventTitle = event.target.value;
-    return localStorage.setItem('event_title', this.eventTitle);
+    this.eventTitle = event.target.value
+    return localStorage.setItem('event_title', this.eventTitle)
   }
 
   private getTitleEvent(): any {
-    this.eventTitle = localStorage.getItem('event_title');
+    this.eventTitle = localStorage.getItem('event_title')
   }
 
   // Date of event
-  private subscription!: Subscription;
 
-  private milliSecondsEvent = 0;
-  public timeToEvent: string = '';
+  private milliSecondsEvent = 0
+  public timeToEvent: string = ''
 
   calculateTimeToEvent() {
-    let milliSecondsNow = Date.now();
+    let milliSecondsNow = Date.now()
 
-    const diff = (this.milliSecondsEvent - milliSecondsNow) / 1000;
-    const days = Math.floor(diff / 86400);
-    const hours = Math.floor((diff % 86400) / 3600);
-    const minutes = Math.floor((diff % 3600) / 60);
-    const seconds = Math.floor(diff % 60);
-    return { days, hours, minutes, seconds };
+    const diff = (this.milliSecondsEvent - milliSecondsNow) / 1000
+    const days = Math.floor(diff / 86400)
+    const hours = Math.floor((diff % 86400) / 3600)
+    const minutes = Math.floor((diff % 3600) / 60)
+    const seconds = Math.floor(diff % 60)
+    return { days, hours, minutes, seconds }
   }
 
   addEvent(type: any, event: MatDatepickerInputEvent<Date>) {
@@ -46,32 +44,23 @@ export class AppComponent implements OnInit, OnDestroy {
         event.value.getDate(),
         event.value.getHours(),
         event.value.getMinutes(),
-        event.value.getSeconds()
-      );
+        event.value.getSeconds(),
+      )
     }
 
-    // TODO: check if this can be in ngOnInit
-    // let { days, hours, minutes, seconds } =
-    // this.calculateTimeToEvent();
-    // this.timeToEvent = `${days}d, ${hours}h, ${minutes}m, ${seconds}s`;
-
     setInterval(() => {
-      let { days, hours, minutes, seconds } = this.calculateTimeToEvent();
-      this.timeToEvent = `${days} days, ${hours}h, ${minutes}m, ${seconds}s`;
-      return localStorage.setItem('time_to_event', this.timeToEvent);
-    }, 1);
+      let { days, hours, minutes, seconds } = this.calculateTimeToEvent()
+      this.timeToEvent = `${days} days, ${hours}h, ${minutes}m, ${seconds}s`
+      return localStorage.setItem('time_to_event', this.timeToEvent)
+    }, 1)
   }
 
   private getTimeToEvent(): any {
-    this.timeToEvent = localStorage.getItem('time_to_event') || '';
+    this.timeToEvent = localStorage.getItem('time_to_event') || ''
   }
 
   ngOnInit(): void {
-    this.getTitleEvent();
-    this.getTimeToEvent();
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.getTitleEvent()
+    this.getTimeToEvent()
   }
 }
